c7b218c3c659a3ce7c318ff153e6d9dfab7af32f
Gard300 <ranis2016gilmanov@gmail.com>
2023-02-08 12:30:05 +0500

    Выполнил правки

--- src/lesson1/Homework1.java
+++ src/lesson1/Homework1.java
@@ -2,10 +2,10 @@ package lesson1;
 
 public class Homework1 {
     public static void main (String args[]) {
-        
+
     }
     public static void task1() {
-        System.out.println("Решение задачи 1");
+        System.out.println("Решение задачи 1!");
     }
 
 }
