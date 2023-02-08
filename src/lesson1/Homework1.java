b1fdf6593a6094563d78523c4dbb323e14adfa38
Gard300 <ranis2016gilmanov@gmail.com>
2023-02-08 12:17:04 +0500

    Выполнил первое дз

---
+++ src/lesson1/Homework1.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class Homework1 {
+    public static void main (String args[]) {
+        
+    }
+    public static void task1() {
+        System.out.println("Решение задачи 1");
+    }
+
+}
